db9be05fba258d9e8d392621f68bbff4940b420d
kieran Sweetman <kikisweetman@gmail.com>
2022-03-05 15:54:53 +0100

    changed/removed  useles functions, starting from scratch and rearranging html for clarity purposes

--- myjs.js
+++ myjs.js
@@ -3,65 +3,27 @@
  document.addEventListener("DOMContentLoaded", function() {
 
     console.log("dom content loaded");
-    
-
-// resseting the animation but this isnt what im looking for
-
-//     const animationEndWords = document.querySelector(".words");
-//     animationEndWords.onanimationend = () => {
-//         animationEndWords.classList.remove("animate__swing");
-//         setTimeout(() => {animationEndWords.classList.add("animate__swing")},50);
-        
-
-//     };
-   
-//     const animationEndCurrency = document.querySelector(".currency");
-//     animationEndCurrency.onanimationend = () => {
-//         animationEndCurrency.classList.remove("animate__swing");
-//         setTimeout(() => {animationEndCurrency.classList.add("animate__swing")},50);
-
-//     };
-// // end
 
 
-// // transition function
-//     function transitionToConverter()  {
+// using inline to test now to keep this file clean.
+// will transfer over onclick actions from html doc to this script 
+//once i have a clear idea of how to organize 
+// the functions and their scope.
 
-//         let check1 = document.querySelector('#btncontrolwords');
-//         let check2 = document.querySelector('#btncontrol');
-//         let ntwElement = document.querySelector('.numberToWord');
-//         let choice_cards = document.querySelector('#choice-cards');
+//PROGRAM
 
-//         // transition
 
-//         function checkboxChecker(element) {
-//             element.addEventListener('change', function() {
-//                 if(element.checked) {
-//                     choice_cards.style.setProperty("animation-play-state", "running");
-//                     setTimeout(() => {choice_cards.style.display = "none"}, 2450);
-//                     ntwElement.style.setProperty("animation-play-state", "running");
-//                 }
-//             });
-//         };
-//         checkboxChecker(check1);
-//         checkboxChecker(check2);
-        
-//     };
-//     transitionToConverter();
-// end
-// 
-//
-   
 // ntw input converter
     const input = document.querySelector("#number_to_format");
-    const log = document.querySelector("#ntf_userOutput");
-    const decimal = document.querySelector('#decimal_formatter')
+    
 
     input.addEventListener("input", function () {
         //number to word formatter 
         writtenNumber.defaults.lang = "fr";
         
         let inputstring = input.value;
+        const log = document.querySelector("#ntf_userOutput");
+        const decimal = document.querySelector('#decimal_formatter');
 
         if (inputstring.indexOf(".") > -1) {
             let newString = inputstring.split(".");
@@ -76,11 +38,5 @@
 
  });
 
- // return to card choices in progress 
- // a test, all of this is not very well coded.
 
-    const goBackButton = document.querySelector("#goBackBtn");
-    goBackButton.addEventListener('click', function () {
-    });
- //end
  });
